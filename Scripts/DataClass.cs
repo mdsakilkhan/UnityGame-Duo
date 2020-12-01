bc8e01eba119ac4a2e14bb6262fe8defba3027f5
Md Sakil Khan <sakil2468@gmail.com>
2020-12-01 08:34:22 -0500

    Add files via upload

---
+++ Scripts/DataClass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataClass : MonoBehaviour {
+
+	public static float score;	
+	public static int shop;
+	public static float volume;
+	
+	void Start() {
+		PlayerData data = SaveSystem.LoadPlayer();
+		score = data.score;
+		shop = data.shop;
+		volume = data.volume;
+	}
+	
+	public float getScore() {
+		return score;
+	}
+	
+	public int getShop() {
+		return shop;
+	}
+	
+	public float getVolume() {
+		return volume;
+	}
+	
+	public void setScore(float data) {
+		score = data;
+		SaveSystem.SavePlayer(this);
+	}
+	
+	public void setShop(int data) {
+		shop = data;
+		SaveSystem.SavePlayer(this);
+	}
+	
+	public void setVolume(float data) {
+		volume = data;
+		SaveSystem.SavePlayer(this);
+	}
+}
