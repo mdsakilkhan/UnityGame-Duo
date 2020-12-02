388371b10e30c7a686e5b2d96ecbf086bc0bb348
Md Sakil Khan <sakil2468@gmail.com>
2020-12-02 16:59:22 -0500

    Update PlayerMovement.cs

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,29 +1,47 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public static float sakil;
+public class PlayerMovement : MonoBehaviour {
+	
+    public static float score;
 	public static bool paused;
+	public float speed;
+	public float rotateSpeed;
+	public bool mobile;
 	
-	void Start() 
-	{	
-		sakil = 0;
+	void Start() {
+		mobile = true; // false for pc control
+		rotateSpeed = 250f;
+		score = 0f;
 		paused = false;
-	}	
+	}
 	
-	void Update()
-    {	
+	void Update() {	
+		speed = CameraMovement.speed;
 		paused = UIControl.paused;
-		if(paused == false){
-			transform.position = new Vector3(transform.position.x,transform.position.y+0.04f,transform.position.z);
-			sakil = transform.position.y;
-			if(Input.GetKey(KeyCode.RightArrow)) {
-				transform.Rotate(new Vector3(0f,0f,-2f));
+		if(!paused){
+			transform.position += Vector3.up * speed * Time.deltaTime;
+			score = transform.position.y;
+			
+			if(mobile) { // touch control
+ 				if (Input.touchCount > 0) {
+					var touch = Input.GetTouch(0);
+					if (touch.position.x < Screen.width/2) {
+						transform.Rotate(new Vector3(0f,0f,rotateSpeed * Time.deltaTime));
+					}
+					else {
+						transform.Rotate(new Vector3(0f,0f,-rotateSpeed * Time.deltaTime));
+					}
+				}
 			}
-			if(Input.GetKey(KeyCode.LeftArrow)) {
-				transform.Rotate(new Vector3(0f,0f,2f));
+			else { // pc control
+				if(Input.GetKey(KeyCode.RightArrow)) {
+					transform.Rotate(new Vector3(0f,0f,-rotateSpeed * Time.deltaTime));
+				}
+				if(Input.GetKey(KeyCode.LeftArrow)) {
+					transform.Rotate(new Vector3(0f,0f,rotateSpeed * Time.deltaTime));
+				}
 			}
 		}
     }
