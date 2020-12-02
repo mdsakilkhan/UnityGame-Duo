7693a013b7504d81a74906510df1d1a35ebc37d3
Md Sakil Khan <sakil2468@gmail.com>
2020-12-02 16:58:49 -0500

    Update CameraMovement.cs

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class CameraMovement : MonoBehaviour
-{	
-	public static bool paused;
+public class CameraMovement : MonoBehaviour {
 	
-	void Start()
-	{
-		transform.position = new Vector3(transform.position.x,transform.position.y+2,transform.position.z);
+	public static bool paused;
+	public static float speed = 5f;
+	public AudioMixer audioMixer;
+	public float volume;
+	
+	void Start() {
+		PlayerData data = SaveSystem.LoadPlayer();
+		volume = data.volume;
+		audioMixer.SetFloat("MusicVol", volume);
+		
+		speed = 5f;
+		transform.position = new Vector3(0f,transform.position.y+2,transform.position.z);
     }
 	
-	void Update()
-    {
+	void Update() {
+		speed += 0.05f * Time.deltaTime;
 		paused = UIControl.paused;
-		if(paused == false){
-			transform.position = new Vector3(transform.position.x,transform.position.y+0.04f,transform.position.z);
+		if(!paused){
+			transform.position += Vector3.up * speed * Time.deltaTime;
 		}
 	}
 }
