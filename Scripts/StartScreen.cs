08af2e7179c49fa5ca260ba39c357a0e4e9f6159
Md Sakil Khan <sakil2468@gmail.com>
2020-12-09 14:17:11 -0500

    Add files via upload

---
+++ Scripts/StartScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    void Start(){
+        SceneManager.LoadScene("Menu");
+    }
+}
